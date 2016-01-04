7d3214ec3279979331bd71004c5433443ffcb834
eakkabinz <eakkabin_it@outlook.co.th>
2016-01-04 22:34:32 +0700

    Update 2016-01-04 22:34

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,3 +1,11 @@
 <?php
 
+\Yii::$app->controller->layout = 'index';
+
 $this->title = 'My Yii Application';
+
+?>
+
+<div class="features_items">
+    <br /><h2 class="title text-center">Not Found.</h2><br />
+</div>
